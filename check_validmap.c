fe74c13353e3a31e5b70d5c1228aa5598a5c2c9a
Dennis Benkhardt <dbenkhar@student.42wolfsburg.de>
2022-01-30 15:57:08 +0100

    still working on valid check map

--- check_validmap.c
+++ check_validmap.c
@@ -6,11 +6,20 @@
 /*   By: dbenkhar <dbenkhar@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 15:53:57 by dbenkhar          #+#    #+#             */
-/*   Updated: 2022/01/30 15:54:24 by dbenkhar         ###   ########.fr       */
+/*   Updated: 2022/01/30 15:56:54 by dbenkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+//	Description:
+//	Meant to check only for first and last line of map.
+//	Return:
+//	Returning 1 if valid 0 if not
+static int	check_easy_lines(t_map *bot)
+{
+
+}
 
 //	Description:
 //	checking either the map is valid or not.
